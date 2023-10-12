5ac1fa5d46f03a95ec20d563c94e467d9dc32800
Sergey Maximenko <maks.sergey.11@gmail.com>
2023-10-12 10:40:34 +0300

    + more function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,72 +21,52 @@ const personalMovieDB = {
 
 
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt ('Один из последних просмотренных фильмов?', ''),
-          b = prompt ('На сколько оцените его?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }            
-}
-
-if (personalMovieDB.count < 10) {
-    console.log('Просмотрено мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Вы классический зритель');
-} else if (personalMovieDB.count >= 30) {
-    console.log('Вы киноман');
-} else {
-    console.log('Произошла ошибка');
-}
-
-console.log(personalMovieDB);
-
-
-
-function sayHello(userName) {
-    let resultPhrase = 'Привет, ';
-    resultPhrase += `${userName}`;
-   return resultPhrase;
-}
-
-console.log(sayHello('John'));
-
-
-function returnNeighboringNumbers(midNumber) {
-    let resultArray = [];
-    for (let i = 0; i <=2; i++) {
-        resultArray[i] = midNumber -1 + i;
-    }
-    return resultArray;
-}
-
-console.log(returnNeighboringNumbers(6));
-
-
-function getMathResult(baseNumber, progressNumber) {
-    let resultStr = ''; 
-    if (typeof(progressNumber || 'number')) {
-        resultStr = `${baseNumber}`;
-        return resultStr;
-    }
-
-    for (let i = 1; i <= progressNumber; i++) {
-        let resNumber = baseNumber * i;
-        if (i === progressNumber) {
-            resultStr += `${resNumber}`;
-        } else {
-            resultStr += `${resNumber}---`;
-        }
-        
-        
-    }
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt ('Один из последних просмотренных фильмов?', ''),
+              b = prompt ('На сколько оцените его?', '');
     
-    return resultStr;
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }            
+    }
 }
 
-console.log(getMathResult(3,10));
+rememberMyFilms();
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
+}
+
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+
+}
+}
+
+writeYourGenres();
